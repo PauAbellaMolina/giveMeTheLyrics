80c2975581038d2619d21d0b664279385be2e79e
Pau Abella <pauabellamolina1@gmail.com>
2021-12-09 11:43:52 +0100

    Quick fix of deployment errors on Netlify

--- src/components/LyricsMain.js
+++ src/components/LyricsMain.js
@@ -18,17 +18,6 @@ function LyricsMain(props) {
 
     /* WHEN COMPONENT MOUNTS -> CALLS THE API */
     useEffect(() => {
-<<<<<<< HEAD
-=======
-        async function fetchData() {
-            const res = await fetch(`https://api.lyrics.ovh/v1/${props.artist}/${props.song}`)
-            res
-            .json()
-            .then(res => setResponse(res))
-            .then(setFetched(true))
-        }
-
->>>>>>> a1528bfe141a9a8265ada1f3409b257a8fca4cf8
         fetchData();
     }, [])
     
